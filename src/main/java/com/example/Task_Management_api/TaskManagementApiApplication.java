522817cb0a644c0cdda6153aff4816552a66d24d
Georgios Charalampidis <36412280+GeorgiosCharalampidis@users.noreply.github.com>
2025-02-01 14:15:12 +0200

    The start of a new era!

---
+++ src/main/java/com/example/Task_Management_api/TaskManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Task_Management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementApiApplication.class, args);
+	}
+
+}
